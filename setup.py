74455cdcc9490bd8bc01bc555567c43f3191924d
Paul Crook <paulcrook726@gmail.com>
2016-11-23 23:40:42 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Gene predicting software.',
+    'author': 'Paul Crook, Saman Azizi',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'paul.crook@uranus.uni-freiburg.de',
+    'version': '0.1a',
+    'install_requires': ['sphinx_rtd_theme'],
+    'name': 'PrognoGene'
+}
+
+setup(**config)
---
+++ src/data_parse.py
@@ -0,0 +1,18 @@
+class DNA:
+    '''
+    This is a test
+    '''
+
+    def test_fun(self):
+        '''
+        This is a test
+
+        :return: True or False
+
+        :rtype: Boolean
+
+        '''
+        if 1 == 0:
+            return False
+        else:
+            return True
