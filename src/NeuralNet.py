9f2173d3d4d8b1b03801fd8061b6e80301032229
Paul Crook <paulcrook726@gmail.com>
2016-12-10 14:43:23 +0100

    Made script for args parsing/processing of gene sequence data

--- src/NeuralNet.py
+++ src/NeuralNet.py
@@ -1,12 +1,6 @@
 import numpy as np
-from Bio import SeqIO
 
 
-def funcheck(item):
-    if item is "A" or "G" or "T" or "C":
-        return True
-    else:
-        return False
 def activation(input_signal):
     """
     This function acts as the cellular activation function.  The sigmoid of input_signal is returned, although
@@ -25,18 +19,6 @@ def activation(input_signal):
 
 
 def activation_d(x):
-    """
-    This is the derivative of the sigmoid function.
-
-    :param output:
-
-    :type output:
-
-    :return:
-
-    :rtype:
-
-    """
 
     return activation(x) * (1.0 - activation(x))  # getting input from output
 
@@ -97,15 +79,12 @@ class NeuralNet(object):
 
         return self.output_values[:]  # returns the calculation of the neural net's effects on the data set
 
-    def back_propogation(self, true_outcomes, learn_rate):
-        if len(true_outcomes) != self.num_outputs:
-            raise Exception("Mismatching input parameters.  Check instance init data and method call data!")
-
+    def back_propogation(self, true_outcome, learn_rate):
         #  calculating error and direction of error between true outcome and predicted outcome layer values
 
         error_slope_out = [0.0] * self.num_outputs
         for output_node in range(self.num_outputs):
-            error = -(true_outcomes[output_node] - self.output_values[output_node])
+            error = -(true_outcome - self.output_values[output_node])
             error_slope_out[output_node] = activation_d(self.output_values[output_node]) * error
 
         #  calculating error and direction of error between output layer and hidden layer
@@ -122,8 +101,7 @@ class NeuralNet(object):
         for hidden_node in range(self.hidden_size):
             for output_node in range(self.num_outputs):
                 change = error_slope_out[output_node] * self.hidden_values[hidden_node]
-                self.output_weight_matrix[hidden_node][output_node] -= learn_rate * \
-                                                                       change + \
+                self.output_weight_matrix[hidden_node][output_node] -= learn_rate * change + \
                                                                        self.output_change[hidden_node][output_node]
                 self.output_change[hidden_node][output_node] = change
 
@@ -132,57 +110,23 @@ class NeuralNet(object):
         for input_node in range(self.num_inputs):
             for hidden_node in range(self.hidden_size):
                 change = error_slope_hid[hidden_node] * self.input_values[input_node]
-                self.input_weight_matrix[input_node][hidden_node] -= learn_rate * \
-                                                                     change + \
+                self.input_weight_matrix[input_node][hidden_node] -= learn_rate * change + \
                                                                      self.input_change[input_node][hidden_node]
                 self.input_change[input_node][hidden_node] = change\
 
         #  calculate new error
 
         error = 0.0
-        for i in range(len(true_outcomes)):
-            error += 0.5 * (true_outcomes[i] - self.output_values[i]) ** 2
+        for i in range(len(self.output_values)):
+            error += 0.5 * (true_outcome - self.output_values[i]) ** 2
 
         return error
 
-    def train(self, training_data, iterations=3000, learn_rate=0.002):
+    def train(self, training_data, iterations=500, learn_rate=0.002):
         for i in range(iterations):
-            error = 0.0
-            for data_point in training_data:
-                inputs = data_point[0]
-                desired = data_point[1]
-                self.feed_forward(inputs)
-                error = self.back_propogation(desired, learn_rate)
+            inputs = training_data[0]
+            desired = training_data[1]
+            self.feed_forward(inputs)
+            error = self.back_propogation(desired, learn_rate)
             if i % 500 == 0:
                 print("Error: " + str(error) + "\n")
-
-with open("coding_sequence_data\chr_1_cs.txt") as f:
-    lines = [line.strip('\n') for line in f]
-str_lines = ''.join(lines)
-coding_genes = str_lines.split(">")
-coding_genes = [item for item in coding_genes if funcheck(item)]
-genes = []
-for gene in coding_genes:
-    gene_data = []
-    for letter in gene:
-        if letter == "A":
-            gene_data.append(0.1260)
-        elif letter == "C":
-            gene_data.append(0.1340)
-        elif letter == "G":
-            gene_data.append(0.0806)
-        elif letter == "T":
-            gene_data.append(0.1335)
-        else:
-            pass
-    genes.append([gene_data])
-    for gene in genes:
-        print(len(gene[0]))
-
-
-
-
-
-
-
-
---
+++ src/prognogene.py
@@ -0,0 +1,87 @@
+import NeuralNet
+import argparse
+import pickle
+import os
+
+NN_VIEW_LENGTH = 100
+
+
+def seq_to_num(letter):
+    if letter == "A":
+        return 0.1260
+    elif letter == "C":
+        return 0.1340
+    elif letter == "G":
+        return 0.0806
+    elif letter == "T":
+        return 0.1335
+    else:
+        return
+
+
+def subdivide_seq(seq_data, n):
+    chunks = []
+    for x in range(0, len(seq_data), n):
+        chunk = seq_data[x:x + n]
+        chunks.append(chunk)
+    return chunks
+
+
+def proc(filename):
+    with open(filename) as f:
+        lines = [line.strip('\n') for line in f if '>' not in line]
+    lines_str = ''.join(lines)
+    seq_list = subdivide_seq([seq_to_num(letter) for letter in list(lines_str)], NN_VIEW_LENGTH)
+    return seq_list
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Feed and utilize a neural network for gene exon prediction',
+                                     prefix_chars='-/')
+    parser.add_argument('seq_path',
+                        action='store',
+                        type=str,
+                        help='Type in the folder containing the desired gene sequence data '
+                             '(either full, or relative path')
+    parser.add_argument('/f',
+                        '-f',
+                        dest='f',
+                        action='store_true',
+                        default=False,
+                        help='This option feeds the neural network the data in "seq_path"')
+    parser.add_argument('/c',
+                        '-c',
+                        dest='c',
+                        action='store_true',
+                        default=False,
+                        help='This option designates whether your learning data corresponds to coding, or non-coding'
+                             'data.  Default is False (coding data)')
+    args = parser.parse_args()
+    path = args.seq_path
+    if args.c is False:
+        c_val = 1
+    else:
+        c_val = 0
+    seq_paths = []
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            seq_paths.append(os.path.join(path, name))
+    if args.f is False:
+        with open('NN.pickle', 'rb') as handle:
+            neural_net = pickle.load(handle)
+        for file in seq_paths:
+            with open(file) as f:
+                print(f)
+    else:
+        if os.path.isfile('NN.pickle') is True:
+            with open('NN.pickle', 'rb') as handle:
+                neural_net = pickle.load(handle)
+        else:
+            neural_net = NeuralNet.NeuralNet(NN_VIEW_LENGTH, 1, 70)
+        for filename in seq_paths:
+            proc_seq = proc(filename)
+            for seq in proc_seq:
+                neural_net.train([seq, c_val])
+
+
+main()
